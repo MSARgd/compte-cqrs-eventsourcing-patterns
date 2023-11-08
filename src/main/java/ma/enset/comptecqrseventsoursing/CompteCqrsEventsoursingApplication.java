03e049c870302db540507291a37fd0a524b04192
sidiAbdalla <sidabdallard@gmail.com>
2023-11-09 00:30:29 +0100

    Thu Nov  9 12:30:29 AM +01 2023

---
+++ src/main/java/ma/enset/comptecqrseventsoursing/CompteCqrsEventsoursingApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.comptecqrseventsoursing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteCqrsEventsoursingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompteCqrsEventsoursingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/enset/comptecqrseventsoursing/CompteCqrsEventsoursingApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.comptecqrseventsoursing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteCqrsEventsoursingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
